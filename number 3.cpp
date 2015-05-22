4cade3d0a3c5abd54b78494ca41c59206bade90d
olexzy <olexy01@gmail.com>
2015-05-22 07:17:08 -0400

    cpp assignment
    Assignment

---
+++ number 3.cpp
@@ -0,0 +1,23 @@
+/*
+* number3.cpp
+* Name: [Afolabi Oladimeyi Gabriel]
+* Department: [Mathematics]
+* Matric No: [45317501HI]
+* A Program that print a box,oval,arrow and a diamond 
+*
+* Stub file to enable you complete assignment#1-question#3
+*/
+#include <iostream>
+
+int main(){
+	std::cout << "\n*********        ***           *             *";
+	std::cout << "\n*       *     *       *       ***           * *";
+	std::cout << "\n*       *    *         *     *****         *   *";
+	std::cout << "\n*       *    *         *       *          *     *";
+	std::cout << "\n*       *    *         *       *         *       *";
+	std::cout << "\n*       *    *         *       *          *     *";
+	std::cout << "\n*       *    *         *       *           *   *";
+	std::cout << "\n*       *     *       *        *            * *";
+	std::cout << "\n*********        ***           *             *";
+}
+
